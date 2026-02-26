2bb1b47cafc9f667fce3e78f3ca1de4a32242e14
Chris Kenst <ckenst@gmail.com>
2026-02-25 17:53:09 -0800

    First test to pull in details from .env

--- tests.js/getInboxMessages.test.js
+++ tests.js/getInboxMessages.test.js
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
-test('GetInboxRequest should return inbox messages', async () => {
+test(`Get top 10 messages for inbox ${process.env.MAILINATOR_INBOX}`, async () => {
     const apiToken = process.env.MAILINATOR_API_TOKEN;
     const domain = process.env.MAILINATOR_RESPONSE_DOMAIN;
     const inbox = process.env.MAILINATOR_INBOX;
@@ -20,10 +20,13 @@ test('GetInboxRequest should return inbox messages', async () => {
 
     const client = new MailinatorClient(apiToken);
 
-    const request = new GetInboxRequest(domain, inbox, 0, 50, Sort.DESC, true);
+    const request = new GetInboxRequest(domain, inbox, 0, 10);
+    // console.log(request);
     const resp = await client.request(request);
+    // console.log(resp);
 
     assert.ok(resp, 'Response should not be null');
     assert.ok(resp.result, 'Response result should be present');
     assert.ok(Array.isArray(resp.result.msgs), 'msgs should be an array');
+    assert.ok(resp.result.msgs.length <= 10, 'Should return no more than 10 messages');
 });
