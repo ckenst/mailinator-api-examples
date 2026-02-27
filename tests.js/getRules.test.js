2c2a0327b8586a84d8cb430eaf3880127745710b
Chris Kenst <ckenst@gmail.com>
2026-02-26 17:50:42 -0800

    A test for getting a list of rules.

--- tests.js/getRules.test.js
+++ tests.js/getRules.test.js
@@ -1,11 +1,34 @@
-test('Get a list of rules for a domain', async () => {
-    // get a list of rules for deletable.testinator.email domain. 
-    // assert that it has 1 rule
-    // we should see a deprecation warning
+import { before, test } from 'node:test';
+import assert from 'node:assert/strict';
+import mailinatorClient from 'mailinator-client';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const {
+    MailinatorClient,
+    GetRulesRequest,
+} = mailinatorClient;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+let client;
+let rulesDomain;
+
+before(() => {
+    const apiToken = process.env.MAILINATOR_API_TOKEN;
+    assert.ok(apiToken, 'MAILINATOR_API_TOKEN is not defined in .env');
+
+    rulesDomain = process.env.MAILINATOR_RULES_DOMAIN;  
+    client = new MailinatorClient(apiToken);
 });
 
-test('Create a rule', async () => {
-    // create a rule for deletable.testinator.email domain.
-    // It will fail because the endpoint is broken
-    // We should see some warning about deprecated endpoint
+test('Get a list of rules for a domain', async () => {
+    const resp = await client.request(new GetRulesRequest(rulesDomain));
+
+    assert.ok(resp.result, 'Response result should be present');
+    assert.ok(resp.result.rules.length >= 1, 'Expected at least 1 rule for this domain');
 });
+
