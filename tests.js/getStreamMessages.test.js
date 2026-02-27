52968f6df2dac819a690539b72451c055e954de9
Chris Kenst <ckenst@gmail.com>
2026-02-26 17:03:12 -0800

    Set test to fail. Not implemented yet

--- tests.js/getStreamMessages.test.js
+++ tests.js/getStreamMessages.test.js
@@ -1,5 +1,11 @@
+import { before, test } from 'node:test';
+import assert from 'node:assert/strict'; 
+import mailinatorClient from 'mailinator-client';      
+import { fail } from 'node:assert';
+
 test('Steam all messages from a domain for 10 seconds', async () => {
     // set throttleInterval to 10 seconds
     // assert the handshake is successful
     // No message needs to be recieved
+    return fail('Not implemented yet in SDK 1.0.9');
 });
